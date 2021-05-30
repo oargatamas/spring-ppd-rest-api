e141ce49b3b512d33466d3326e3557be5695eafe
toarga <oarga.tamas@gmail.com>
2021-05-30 16:53:39 +0200

    Spring initalizr

---
+++ src/main/java/hu/medev/examples/ppdspringrestapi/PpdSpringRestApiApplication.java
@@ -0,0 +1,13 @@
+package hu.medev.examples.ppdspringrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpdSpringRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpdSpringRestApiApplication.class, args);
+	}
+
+}
