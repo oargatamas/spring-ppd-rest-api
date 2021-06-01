76f143d0034c7274f4ddf7a30ca2e83a62a0280a
toarga <oarga.tamas@gmail.com>
2021-06-01 20:17:17 +0200

    bugfix.

--- src/main/java/hu/medev/examples/ppdspringrestapi/model/paging/PagingDetails.java
+++ src/main/java/hu/medev/examples/ppdspringrestapi/model/paging/PagingDetails.java
@@ -2,8 +2,10 @@ package hu.medev.examples.ppdspringrestapi.model.paging;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.springframework.hateoas.RepresentationModel;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 public class PagingDetails extends RepresentationModel<PagingDetails> {
     private int pageNumber;
